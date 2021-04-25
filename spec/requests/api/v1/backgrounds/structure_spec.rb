28ec0789df75ef2ff30471ddec2c2432af839abf
Alex Schwartz <aschwartz@detaso.com>
2021-04-24 22:07:11 -0500

    Add functioning happy path

--- spec/requests/api/v1/backgrounds/structure_spec.rb
+++ spec/requests/api/v1/backgrounds/structure_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Backgrounds' do
   describe 'General Structure' do
     it 'is correct' do
-      VCR.use_cassette('denver_background_request') do
-        get api_v1_background_path(location: 'denver,co')
+      VCR.turn_off!
+      WebMock.allow_net_connect!
+
+      # VCR.use_cassette('denver_background_request') do
+        get api_v1_backgrounds_path(location: 'denver,co')
 
         expect(response).to be_successful
         body = JSON.parse(response.body, symbolize_names: true)
@@ -22,7 +25,7 @@ RSpec.describe 'Backgrounds' do
         expect(attributes.keys).to eq([:image, :credit])
 
         image = attributes[:image]
-        expect(image.keys).to eq([:id, :width, :height, :location, :title, :urls])
+        expect(image.keys).to eq([:width, :height, :urls])
         urls = image[:urls]
         expect(urls.keys).to eq([:raw, :full, :regular, :small, :thumb])
 
@@ -32,52 +35,50 @@ RSpec.describe 'Backgrounds' do
         host = credit[:host]
         expect(host.keys).to eq([:name, :url])
       end
-    end
+    # end
   end
 
   describe 'sad path' do
     it 'returns error if query params are not sent' do
-      get api_v1_background_path
+      get api_v1_backgrounds_path
       expect(response).to have_http_status(400)
     end
 
     it 'returns error if location param is not the right format' do
-      get api_v1_background_path(location: 'denver,colorado')
+      get api_v1_backgrounds_path(location: 'denver,colorado')
       expect(response).to have_http_status(400)
     end
   end
-
-  def structure
-    {
-      id: nil,
-      type: image,
-      attributes: {
-        image: {
-          id: 1,
-          width: 444,
-          height: 222,
-          location: 'denver, co',
-          title: 'buildings at midday',
-          urls: {
-            raw: 'https://unsplash.com/photo/path?utm_source=sweater_weather&utm_medium=referral',
-            full: 'https://unsplash.com/photo/path?utm_source=sweater_weather&utm_medium=referral',
-            regular: 'https://unsplash.com/photo/path?utm_source=sweater_weather&utm_medium=referral',
-            small: 'https://unsplash.com/photo/path?utm_source=sweater_weather&utm_medium=referral',
-            thumb: 'https://unsplash.com/photo/path?utm_source=sweater_weather&utm_medium=referral'
-          }
-        },
-        credit: {
-          author: {
-            name: 'Annie Spratt',
-            url: 'https://unsplash.com/@anniespratt?utm_source=sweater_weather&utm_medium=referral'
-          },
-          host: {
-            name: 'Unsplash',
-            url: 'https://unsplash.com?utm_source=sweater_weather&utm_medium=referral'
-          }
-        }
-      }
-    }
-  end
 end
 
+=begin expected response structure
+{
+  id: nil,
+  type: image,
+  attributes: {
+    image: {
+      width: 444,
+      height: 222,
+      location: 'denver, co',
+      title: 'buildings at midday',
+      urls: {
+        raw: 'https://unsplash.com/photo/path?utm_source=sweater_weather&utm_medium=referral',
+        full: 'https://unsplash.com/photo/path?utm_source=sweater_weather&utm_medium=referral',
+        regular: 'https://unsplash.com/photo/path?utm_source=sweater_weather&utm_medium=referral',
+        small: 'https://unsplash.com/photo/path?utm_source=sweater_weather&utm_medium=referral',
+        thumb: 'https://unsplash.com/photo/path?utm_source=sweater_weather&utm_medium=referral'
+      }
+    },
+    credit: {
+      author: {
+        name: 'Annie Spratt',
+        url: 'https://unsplash.com/@anniespratt?utm_source=sweater_weather&utm_medium=referral'
+      },
+      host: {
+        name: 'Unsplash',
+        url: 'https://unsplash.com?utm_source=sweater_weather&utm_medium=referral'
+      }
+    }
+  }
+}
+=end
