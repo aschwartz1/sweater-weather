c41315d9e0722dd2d13be97c7ba0e21d557095ec
Alex Schwartz <aschwartz@detaso.com>
2021-04-25 21:57:04 -0500

    Update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.unique.email }
     password { Faker::Appliance.equipment }
+
+    trait :test do
+      email { 'test@example.com' }
+      password { 'password' }
+    end
   end
 end
