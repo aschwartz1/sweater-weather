ce1d1dde14c6334adcff1e540cba7134a721cfc3
Alex Schwartz <aschwartz@detaso.com>
2021-04-26 10:25:31 -0500

    Add vcr allow connections config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,7 @@ SimpleCov.add_filter(['spec', 'config'])
 
 require 'webmock/rspec'
 VCR.configure do |config|
+  config.allow_http_connections_when_no_cassette = true
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
 end
