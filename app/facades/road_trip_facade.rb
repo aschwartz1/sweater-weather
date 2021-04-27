01a69d1c61385534d71ece251d3733408bf6f86e
Alex Schwartz <aschwartz@detaso.com>
2021-04-27 11:09:26 -0500

    Update facade with dream driven weather service calls

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,22 +1,23 @@
 require_relative '../services/map_service'
+require_relative '../services/weather_service'
 
 class RoadTripFacade
   def self.create_trip(origin, destination)
     directions = fetch_directions(origin, destination)
-    weather = fetch_destination_weather(directions.to[:latitude], directions.to[:longitude], directions.travel_time)
+    eta_weather = fetch_eta_weather(directions.to[:latitude], directions.to[:longitude], directions.travel_time)
 
-    format_response(directions, weather)
+    format_response(directions, eta_weather)
   end
 
   private_class_method
 
-  def self.format_response(directions, weather)
+  def self.format_response(directions, eta_weather)
     OpenStruct.new({
       id: nil,
       start_city: directions.from[:name],
       end_city: directions.to[:name],
       travel_time: format_travel_time(directions.travel_time),
-      weather_at_eta: weather
+      weather_at_eta: eta_weather
     })
   end
 
@@ -24,11 +25,33 @@ class RoadTripFacade
     MapService.fetch_directions(from: origin, to: destination)
   end
 
-  def self.fetch_destination_weather(lat, lng, travel_time)
+  def self.fetch_eta_weather(lat, lng, travel_time)
+    hours = travel_time.split(':').first.to_i
+    scope = days_or_hours(hours)
+    results_needed = (hours % 48) + 1
+
+    data = fetch_weather_for_scope(lat, lng, scope, results_needed)
+    eta_weather = data.last
+
+    trim_eta_weather(eta_weather)
+  end
+
+  def self.trim_eta_weather(data)
     {
-      temperature: lat + lng,
-      conditions: "not yet implemented #{travel_time}"
+      temperature: data.temperature,
+      conditions: data.conditions
     }
+  end
+
+  def self.fetch_weather_for_scope(lat, lng, scope, results_needed)
+    return WeatherService.fetch_hourly(lat, lng, results_needed) if scope == :hours
+
+    WeatherService.fetch_daily(lat, lng, results_needed)
+  end
+
+  def self.days_or_hours(hours)
+    return :hours if hours < 48
+    :days
   end
 
   def self.format_travel_time(time)
