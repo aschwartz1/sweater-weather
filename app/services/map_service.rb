516ce300b56b157707a92fa84ccec8506776d3dc
Alex Schwartz <aschwartz@detaso.com>
2021-04-27 00:03:00 -0500

    Add directions to map service

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -1,14 +1,25 @@
 class MapService
   def self.fetch_geocoords(location)
-    response = map_connection.get('address') do |req|
+    response = map_connection.get('geocoding/v1/address') do |req|
       req.params[:location] = location
     end
 
     format_map_response(response)
   end
 
+  def self.fetch_directions(from:, to:)
+    response = map_connection.get('directions/v2/route') do |req|
+      req.params[:from] = from
+      req.params[:to] = to
+    end
+
+    format_directions(response)
+  end
+
+  private
+
   def self.map_connection
-    @map_connection ||= Faraday.new 'http://www.mapquestapi.com/geocoding/v1' do |conn|
+    @map_connection ||= Faraday.new 'http://www.mapquestapi.com' do |conn|
       conn.params[:key] = ENV['mapquest_key']
     end
   end
@@ -20,6 +31,26 @@ class MapService
     OpenStruct.new({
       latitude: geocoords[:lat],
       longitude: geocoords[:lng]
+    })
+  end
+
+  def self.format_directions(response)
+    body = JSON.parse(response.body, symbolize_names: true)
+    route = body[:route]
+    locations = route[:locations]
+
+    OpenStruct.new({
+      from: {
+        name: "#{locations[0][:adminArea5]}, #{route[:locations][0][:adminArea3]}",
+        latitude: locations[0][:latLng][:lat],
+        longitude: locations[0][:latLng][:lng]
+      },
+      to: {
+        name: "#{locations[1][:adminArea5]}, #{route[:locations][1][:adminArea3]}",
+        latitude: locations[1][:latLng][:lat],
+        longitude: locations[1][:latLng][:lng]
+      },
+      travel_time: route[:formattedTime]
     })
   end
 end
--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -3,17 +3,50 @@ require_relative '../../app/services/map_service.rb'
 require 'rails_helper'
 
 RSpec.describe 'Forecast Service' do
-  describe 'Fetch Geocoords' do
+  describe 'Geocoords for city' do
     describe 'happy path' do
       it 'returns correct structure and data' do
-        result = MapService.fetch_geocoords('denver,co')
+        VCR.use_cassette('denver_geocoords') do
+          geocoords = MapService.fetch_geocoords('denver,co')
 
-        expect(result).to be_an(OpenStruct)
-        expect(result).to respond_to(:latitude)
-        expect(result).to respond_to(:longitude)
+          expect(geocoords).to be_an(OpenStruct)
+          expect(geocoords).to respond_to(:latitude)
+          expect(geocoords).to respond_to(:longitude)
 
-        expect(result[:latitude]).to be_between(39, 40)
-        expect(result[:longitude]).to be_between(-105, -104)
+          expect(geocoords[:latitude]).to be_between(39, 40)
+          expect(geocoords[:longitude]).to be_between(-105, -104)
+        end
+      end
+    end
+  end
+
+  describe 'Directions' do
+    describe 'happy path' do
+      it 'returns correct structure and data' do
+        VCR.use_cassette('portland_to_miami') do
+          directions = MapService.fetch_directions(from: 'portland,or', to: 'miami,fl')
+
+          expect(directions).to be_an(OpenStruct)
+          expect(directions).to respond_to(:from)
+          expect(directions).to respond_to(:to)
+          expect(directions).to respond_to(:travel_time)
+
+          expect(directions.from).to be_a(Hash)
+          expect(directions.from.keys).to eq([:name, :latitude, :longitude])
+          expect(directions.from[:name]).to eq('Portland, OR')
+          expect(directions.from[:latitude]).to be_between(45, 46)
+          expect(directions.from[:longitude]).to be_between(-123, -122)
+
+          expect(directions.to).to be_a(Hash)
+          expect(directions.to.keys).to eq([:name, :latitude, :longitude])
+          expect(directions.to[:name]).to eq('Miami, FL')
+          expect(directions.to[:latitude]).to be_between(25, 26)
+          expect(directions.to[:longitude]).to be_between(-81, -80)
+
+          expect(directions.travel_time).to be_a(String)
+          # Expecting the format HH:MM:SS
+          expect(directions.travel_time.split(':').size).to eq(3)
+        end
       end
     end
   end
